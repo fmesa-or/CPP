07c4bd9f6ab8898e7dea21fe43064b4e5f36cad7
fmesa-or <franciscomesaempleo@gmail.com>
2025-11-17 14:44:37 +0100

    CPP01/ex03 -Main finished.

--- CPP01/ex03/src/main.cpp
+++ CPP01/ex03/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 12:32:18 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/11/17 13:14:00 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/11/17 14:44:11 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,18 @@ int	main()
 
 		HumanA	bob("Bob", club);
 		bob.attack();
-		
-		
+		club.setType("some other type of club");
+		bob.attack();
 	}
+	{
+		Weapon	club = Weapon("crude spiked club");
+
+		HumanB	jim("Jim");
+		jim.setWeapon(club);
+		jim.attack();
+		club.setType("some other type of club");
+		jim.attack();
+	}
+
+	return 0;
 }
