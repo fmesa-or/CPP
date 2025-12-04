4ff3703038f194b7a3db8f5dc3d0eef6daebfa92
fmesa-or <franciscomesaempleo@gmail.com>
2025-12-04 13:15:56 +0100

    CPP03 DONE & DOC -ex02 DONE & DOC

--- CPP03/ex01/src/main.cpp
+++ CPP03/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 13:05:40 by fmesa-or          #+#    #+#             */
-/*   Updated: 2025/12/03 13:50:43 by fmesa-or         ###   ########.fr       */
+/*   Updated: 2025/12/04 13:07:09 by fmesa-or         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,31 +15,30 @@
 
 int	main() {
 	std::cout << CI << "\n=== Creating ClapTraps ===\n" << RES << std::endl;
-	 ClapTrap	ct00;
-	 ClapTrap	ct01("Boby");
-	 ClapTrap	ct02(ct01);
+	ClapTrap	ct00;
+	ClapTrap	ct01("Boby");
+	ClapTrap	ct02(ct01);
 
-	 ct00 = ClapTrap ("Miguel");
-	 
-	 std::cout << CI << "\n=== Creating ScavTraps ===\n" << RES << std::endl;
-	 ScavTrap	st00;
-	 ScavTrap	st01("Mike Sinaloa");
-	 ScavTrap	st02(st01);
+	ct00 = ClapTrap ("Miguel");
 
-	 st00 = ScavTrap("Furiosa");
+	std::cout << CI << "\n=== Creating ScavTraps ===\n" << RES << std::endl;
+	ScavTrap	st00;
+	ScavTrap	st01("Mike Sinaloa");
+	ScavTrap	st02(st01);
 
-	 std::cout << CI << "\n=== Testing guardGate ===\n" << RES << std::endl;
-	 st00.guardGate();
+	st00 = ScavTrap("Furiosa");
 
-	 std::cout << CI << "\n=== ClapTrap attack (should print ClapTrap message) ===\n" << RES << std::endl;
-	 ct00.attack("Furiosa");
-	 st01.takeDamage(ct00.getDamage());
-	 
-	 std::cout << CI << "\n=== ScavTrap attack (should print ScavTrap message) ===\n" << RES << std::endl;
-	 st00.attack("Miguel");
-	 ct00.takeDamage(st00.getDamage());
+	std::cout << CI << "\n=== Testing guardGate ===\n" << RES << std::endl;
+	st00.guardGate();
 
-	 std::cout << CI << "\n=== Destructors ===\n" << RES << std::endl;
-	 return 0;
+	std::cout << CI << "\n=== ClapTrap attack (should print ClapTrap message) ===\n" << RES << std::endl;
+	ct00.attack("Furiosa");
+	st01.takeDamage(ct00.getDamage());
+
+	std::cout << CI << "\n=== ScavTrap attack (should print ScavTrap message) ===\n" << RES << std::endl;
+	st00.attack("Miguel");
+	ct00.takeDamage(st00.getDamage());
+
+	std::cout << CI << "\n=== Destructors ===\n" << RES << std::endl;
 	return 0;
 }
---
+++ CPP03/ex02/include/ScavTrap.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmesa-or <fmesa-or@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 12:29:26 by fmesa-or          #+#    #+#             */
+/*   Updated: 2025/12/03 13:44:52 by fmesa-or         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+#define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap {
+	public:
+		ScavTrap(void);
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap& operator=(const ScavTrap& other);
+		~ScavTrap(void);
+
+		void	attack(const std::string& target);
+		void	guardGate(void);
+};
+
+#endif
